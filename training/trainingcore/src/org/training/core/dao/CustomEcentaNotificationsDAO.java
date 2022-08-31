81566e19f8e097e096b04e270e822796c5427372
VenkatRaja-Ch <chvenkatraja.work@gmail.com>
2022-08-31 11:50:59 +0300

    removed the redundant part of the code for CustomEcentaNotificationsDAO

--- training/trainingcore/src/org/training/core/dao/CustomEcentaNotificationsDAO.java
+++ training/trainingcore/src/org/training/core/dao/CustomEcentaNotificationsDAO.java
@@ -10,18 +10,18 @@ import java.util.List;
 public interface CustomEcentaNotificationsDAO{
 
     // Task: 4 -> CronJobs
-    public List<EcentaNotificationModel> findAllNotificationOlderThanSpecificDays(final Date oldDate);
+    List<EcentaNotificationModel> findAllNotificationOlderThanSpecificDays(final Date oldDate);
 
     // Task: 6 -> DAO
     /* Finds the B2BCustomer using the passed uid */
-    public List<B2BCustomerModel> findB2BCustomerUsingUID(final String b2bCustomerUid);
+    List<B2BCustomerModel> findB2BCustomerUsingUID(final String b2bCustomerUid);
 
     /* Find the Ecenta Notification for the passed B2B Customer */
-    public List<EcentaNotificationModel> findAllNotificationForSpecificB2bCustomer(B2BCustomerModel b2BCustomerModel);
+    List<EcentaNotificationModel> findAllNotificationForSpecificB2bCustomer(B2BCustomerModel b2BCustomerModel);
 
     /* Finds the Ecenta Notification for the passed B2B Customer with Specific Type */
-    public List<EcentaNotificationModel> findAllNotificationForSpecificB2bCustomerAndSpecificType(B2BCustomerModel b2BCustomerModel, String ecentaNotificationType);
+    List<EcentaNotificationModel> findAllNotificationForSpecificB2bCustomerAndSpecificType(B2BCustomerModel b2BCustomerModel, String ecentaNotificationType);
 
     /* Finds the Ecenta Notification for the passed B2B Customer with Specific Priority */
-    public List<EcentaNotificationModel> findAllNotificationForSpecificB2bCustomerAndSpecificPriority(B2BCustomerModel b2BCustomerModel, String ecentaNotificationPriority);
+    List<EcentaNotificationModel> findAllNotificationForSpecificB2bCustomerAndSpecificPriority(B2BCustomerModel b2BCustomerModel, String ecentaNotificationPriority);
 }
