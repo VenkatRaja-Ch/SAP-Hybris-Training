bf0845f9f74f49e794affffe7bc2d0c79fa7216e
VenkatRaja-Ch <chvenkatraja.work@gmail.com>
2022-08-31 11:50:07 +0300

    Implemented the methods for CustomEcentaNotificationCRUDServices using ModelService

--- training/trainingcore/src/org/training/core/crud/impl/CustomEcentaNotificationCRUDServiceImpl.java
+++ training/trainingcore/src/org/training/core/crud/impl/CustomEcentaNotificationCRUDServiceImpl.java
@@ -1,64 +1,45 @@
 package org.training.core.crud.impl;
 
-import de.hybris.platform.servicelayer.exceptions.ModelRemovalException;
 import de.hybris.platform.servicelayer.model.ModelService;
-import de.hybris.platform.tx.Transaction;
 
-import org.springframework.util.CollectionUtils;
 import org.training.core.crud.CustomEcentaNotificationCRUDService;
 import org.training.core.model.EcentaNotificationModel;
-import org.apache.log4j.Logger;
 
-import java.util.List;
+import java.util.Objects;
 
 public class CustomEcentaNotificationCRUDServiceImpl implements CustomEcentaNotificationCRUDService {
 
     private ModelService modelService;
-    private final static Logger LOG = Logger.getLogger(CustomEcentaNotificationCRUDServiceImpl.class.getName());
 
     @Override
-    public List<EcentaNotificationModel> createEcentaNotification() {
-        return null;
+    public EcentaNotificationModel createEcentaNotification() {
+
+        return getModelService().create(EcentaNotificationModel.class);
     }
 
     @Override
-    public List<EcentaNotificationModel> updateEcentaNotification() {
-        return null;
-    }
+    public void updateEcentaNotification(final EcentaNotificationModel currentEcentaNotificationItem){
 
-    @Override
-    public List<EcentaNotificationModel> cloneEcentaNotification() {
-        return null;
-    }
-
-    @Override
-    public List<EcentaNotificationModel> deleteEcentaNotification() {
-        return null;
-    }
-
-    private void commitCurrentTransaction(List<EcentaNotificationModel> ecentaNotificationModelList)
-    {
-        if(!(CollectionUtils.isEmpty(ecentaNotificationModelList)))
-        {
-            Transaction tx = null;
-
-            try
-            {
-                tx = Transaction.current();
-                tx.begin();
-                getModelService().saveAll(ecentaNotificationModelList);
-                tx.commit();
-            }
-            catch( final ModelRemovalException e)
-            {
-                if(!(null == tx))
-                    tx.rollback();
-
-                LOG.error("Error occurred while committing the current Transaction");
-            }
+        if(!Objects.isNull(currentEcentaNotificationItem)) {
+            getModelService().save(currentEcentaNotificationItem);
         }
 
-        LOG.info("\n\ncommitCurrentTransaction() has been executed. \n\n");
+    }
+
+    @Override
+    public EcentaNotificationModel cloneEcentaNotification(final EcentaNotificationModel currentEcentaNotificationItem) {
+
+        if(!Objects.isNull(currentEcentaNotificationItem))
+            return getModelService().clone(currentEcentaNotificationItem);
+
+        return null;
+    }
+
+    @Override
+    public void deleteEcentaNotification(final EcentaNotificationModel currentEcentaNotificationItem) {
+
+        if(!Objects.isNull(currentEcentaNotificationItem))
+            getModelService().remove(currentEcentaNotificationItem);
     }
 
     /* GETTERS & SETTERS */
