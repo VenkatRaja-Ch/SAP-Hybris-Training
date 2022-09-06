52d18b5156f330aef2cb2429e747939832b64457
VenkatRaja-Ch <chvenkatraja.work@gmail.com>
2022-09-06 15:17:55 +0300

    Updated the cloneEcentaNotification method.
    Added try-catch functionality to it to catch the exceptions.
    
    updated the method to return the cloned EcentaNotification with new unique id set to it as "cloneItem_" with the current date and time.

--- training/trainingcore/src/org/training/core/crud/impl/CustomEcentaNotificationCRUDServiceImpl.java
+++ training/trainingcore/src/org/training/core/crud/impl/CustomEcentaNotificationCRUDServiceImpl.java
@@ -1,15 +1,20 @@
 package org.training.core.crud.impl;
 
+import de.hybris.platform.core.GenericSearchConstants;
 import de.hybris.platform.servicelayer.model.ModelService;
 
+import org.apache.log4j.Logger;
 import org.training.core.crud.CustomEcentaNotificationCRUDService;
+import org.training.core.job.EcentaNotificationsRemovalJob;
 import org.training.core.model.EcentaNotificationModel;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class CustomEcentaNotificationCRUDServiceImpl implements CustomEcentaNotificationCRUDService {
 
     private ModelService modelService;
+    private final static Logger LOG = Logger.getLogger(EcentaNotificationsRemovalJob.class.getName());
 
     @Override
     public EcentaNotificationModel createEcentaNotification() {
@@ -20,19 +25,23 @@ public class CustomEcentaNotificationCRUDServiceImpl implements CustomEcentaNoti
     @Override
     public void updateEcentaNotification(final EcentaNotificationModel currentEcentaNotificationItem){
 
-        if(!Objects.isNull(currentEcentaNotificationItem)) {
+        if(!Objects.isNull(currentEcentaNotificationItem))
+        {
             getModelService().save(currentEcentaNotificationItem);
         }
-
     }
 
     @Override
-    public EcentaNotificationModel cloneEcentaNotification(final EcentaNotificationModel currentEcentaNotificationItem) {
+    public void cloneEcentaNotification(final EcentaNotificationModel currentEcentaNotificationItem) {
 
-        if(!Objects.isNull(currentEcentaNotificationItem))
-            return getModelService().clone(currentEcentaNotificationItem);
-
-        return null;
+        try {
+            if (!Objects.isNull(currentEcentaNotificationItem)) {
+                getModelService().clone(currentEcentaNotificationItem).setId("cloneItem_" + LocalDateTime.now());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            LOG.info("\n\n\nError caught\n\n\n");
+        }
     }
 
     @Override
